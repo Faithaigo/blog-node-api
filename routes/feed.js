a0692fb6bc4c4434cc5e165902626d2115a8eee8
faith <faith@marascientific.org>
2024-06-10 09:10:51 +0300

    Update route path

--- routes/feed.js
+++ routes/feed.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const feedController = require('../conntrollers/feed')
+const feedController = require('../controllers/feed')
 
 router.get('/posts', feedController.getPosts)
 
