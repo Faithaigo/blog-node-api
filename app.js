422d9337bc8a1e634add9a0ac96fe292952c5596
faith <faith@marascientific.org>
2024-06-10 09:29:44 +0300

    Add CORS

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const app = express()
 
 app.use(bodyParser.json())
 
+/**
+ * Set headers on any response that leaves the server (CORS)
+ */
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*') //allow access from any domain
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST, PUT, PATCH, DELETE') //allow specific http methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization') //allow headers clients set
+    next()
+})
+
 app.use('/feed', feedRoutes)
 
 app.listen(8080)
