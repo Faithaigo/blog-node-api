553ff0664a78074b20dfdf5a1d07059ef59943c9
faith <faith@marascientific.org>
2024-06-26 12:42:51 +0300

    Add pagination

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -108,19 +108,29 @@ module.exports = {
       updatedAt: savedPost.updatedAt.toISOString(),
     };
   },
-  getPosts: async function (args, req) {
+  getPosts: async function ({ page }, req) {
     if (!req.isAuth) {
       const error = new Error("Not authenticated");
       error.code = 401;
       throw error;
     }
+    if (!page) {
+      page = 1;
+    }
+    const perPage = 2;
     const totalPosts = await Post.find().countDocuments();
-    const posts = await Post.find().sort({ createdAt: -1 }).populate("creator");
-    return {posts:posts.map(item=>({
-      ...item._doc,
-      _id: item._id.toString(),
-      createdAt: item.createdAt.toISOString(),
-      updatedAt: item.updatedAt.toISOString(),
-    })), totalPosts };
+    const posts = await Post.find().sort({ createdAt: -1 })
+    .skip((page-1) * perPage)
+    .limit(perPage)
+    .populate("creator");
+    return {
+      posts: posts.map((item) => ({
+        ...item._doc,
+        _id: item._id.toString(),
+        createdAt: item.createdAt.toISOString(),
+        updatedAt: item.updatedAt.toISOString(),
+      })),
+      totalPosts,
+    };
   },
 };
